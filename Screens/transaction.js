d7540512eb467e13703e0af1ce7e8c3db9e452fa
Arnav2401 <77386547+Arnav2401@users.noreply.github.com>
2021-07-06 17:11:00 +0530

    Add files via upload

---
+++ Screens/transaction.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Transaction extends React.Component{
+  render(){
+    return(
+      <Text>This is a second Screen</Text>
+    )
+  }
+}
